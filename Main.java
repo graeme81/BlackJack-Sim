14be6487621503adac810293bfdae124f3011e96
graeme81 <g.cameron@hotmail.co.uk>
2017-08-29 13:31:58 +0100

    tracking wins

--- Main.java
+++ Main.java
@@ -5,9 +5,10 @@ public class Main {
 		
 		Player player = new Player();
 		Player dealer = new Player();
-		int rotations = 1000;
+		int rotations = 1000000;
 		int stop = 0;
 		int win = 0, total = 0;
+		int atOne = 0, atTwo = 0, atThree = 0, atFour = 0, beat = 0;
 		
 		
 		for (int i = 0; i < rotations; i++){
@@ -21,18 +22,23 @@ public class Main {
 			switch(stop){
 				case 1:
 					win = 5;
+					atOne++;
 					break;
 				case 2:
 					win = 10;
+					atTwo++;
 					break;
 				case 3:
 					win = 15;
+					atThree++;
 					break;
 				case 4:	case 5:	case 6:	case 7:	case 8:
 					win = 20;
+					atFour++;
 					break;
 				case 9:
 					win = -2380;
+					beat++;
 					break;
 				
 			}
@@ -40,6 +46,7 @@ public class Main {
 			total = total+win;
 		}
 		System.out.println("Total win after "+rotations+" rotations is: £"+total);
+		System.out.println("One: " + atOne + ", Two: " + atTwo + ", Three: "+ atThree + ", Four: "+ atFour + ", BEAT: " + beat);
 	}
 
 }
